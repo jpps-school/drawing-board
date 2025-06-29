faba1ff90560ea56be8b08bea80b9cc53e0b7d4e
jpps-school <tonystark65648@gmail.com>
2025-06-29 19:37:09 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', (e) => {
+  console.log('Service Worker Installed');
+  e.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/index.css',
+        '/contact.html',
+        '/images/jpps_logo.jpg',
+        '/resources.html',
+        '/create.html',
+        '/about.html',
+        '/gallery.html'        
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
